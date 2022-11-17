adaf734881506fb2ca2a5a0f5178d0592d0dce5c
Thyriion <kevinschueler2000@gmail.com>
2022-11-17 19:56:35 +0100

    Animation für Feedback

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import feedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
@@ -8,23 +9,23 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              layout
+            >
+              <FeedbackItem item={item} handleDelete={handleDelete} />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
