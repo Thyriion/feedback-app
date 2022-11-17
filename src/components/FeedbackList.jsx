8e344899fdabfef6df9bf6848e76a50ca812149e
Thyriion <kevinschueler2000@gmail.com>
2022-11-17 20:12:09 +0100

    refactor

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -10,22 +10,30 @@ function FeedbackList({ feedback, handleDelete }) {
   return (
     <div className="feedback-list">
       <AnimatePresence>
-        {feedback.map((item) => {
-          return (
-            <motion.div
-              key={item.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              layout
-            >
-              <FeedbackItem item={item} handleDelete={handleDelete} />
-            </motion.div>
-          );
-        })}
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <FeedbackItem item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
       </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem item={item} handleDelete={handleDelete} key={item.id} />
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 export default FeedbackList;
