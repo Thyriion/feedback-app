fcda0a0d3e65dd7ba105cd4f0d4d1e70177fb6f7
Thyriion <kevinschueler2000@gmail.com>
2022-11-19 10:19:03 +0100

    Vorbereitung für json-server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 // import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
@@ -29,7 +31,9 @@ function FeedbackList() {
   //   </div>
   // );
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => {
         return <FeedbackItem item={item} key={item.id} />;
