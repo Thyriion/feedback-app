2bcbe8f3279978d1e9c122fb3691d258350d9da7
Thyriion <kevinschueler2000@gmail.com>
2022-11-21 21:06:12 +0100

    .

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -16,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
 
   const fetchFeedback = async () => {
     const response = await fetch("/feedback?_sort=id&_order=desc");
-    console.log(response);
+    console.log(await response.json());
     // const data = await response.json();
     //
     // setFeedback(data);
